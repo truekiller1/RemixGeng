41d14a76afa35d3da1561749f1f88d0b44ab514a
truekiller1 <69351236+truekiller1@users.noreply.github.com>
2021-04-08 09:25:50 +0530

    Update and rename fuk.py to fook.py

--- userbot/modules/fook.py
+++ userbot/modules/fook.py
@@ -1,12 +1,12 @@
 # (c) @Unknown
-# Original written by @Unknown edit by @Unbornkiller
+# Original written by @Unknown edit by @sudo_zeref
 
 from telethon import events
 import asyncio
 from collections import deque
 from userbot.events import register
 
-@register(outgoing=True, pattern="^.fuk$")
+@register(outgoing=True, pattern="^.fook$")
 async def fuck(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("`\n╱┏━━━┓.. ┏┓╱┏┓╭━━━╮ ╱╱┏┓ `"
